3fb27ec8dd860ff5c448baa94d47a22824b50050
FranciscoAraujoPT <quico.calado.araujo@gmail.com>
2022-01-22 19:03:30 +0000

    Done query number 1

--- Entrega3/queries/int1.sql
+++ Entrega3/queries/int1.sql
@@ -1,8 +1,32 @@
 .mode columns
 .headers on
-.nullvalue NULL 
+.nullvalue NULL
 
--- Id of the player with the highest number of elo points.
+-- A diferença de pontos dos jogadores e a média num torneio aleatório.
+CREATE VIEW IF NOT EXISTS randomIdTournament as
+    SELECT id_tournament
+    FROM Tournament
+    LIMIT 1 OFFSET (ABS(RANDOM()) % MAX((SELECT COUNT(*) FROM Tournament), 1));
 
-SELECT id_player, MAX(number_of_elo_points) as GreatestPlayer
-FROM FideRanking
+CREATE VIEW IF NOT EXISTS allPlayersInTournament as
+    SELECT id_player
+    FROM PlayerTournament, randomIdTournament
+    WHERE PlayerTournament.id_tournament = randomIdTournament.id_tournament;
+
+Create VIEW IF NOT EXISTS playersRanking as
+    SELECT first_name, last_name, number_of_elo_points
+    FROM Player, allPlayersInTournament, FideRanking
+    WHERE allPlayersInTournament.id_player = FideRanking.id_player and Player.id_player = allPlayersInTournament.id_player;
+
+Create VIEW IF NOT EXISTS average as
+    SELECT round(AVG(number_of_elo_points),0) as average
+    FROM playersRanking;
+
+SELECT first_name, last_name, number_of_elo_points, number_of_elo_points-average as average_of_fide_points
+FROM playersRanking, average
+ORDER BY average_of_fide_points desc;
+
+DROP VIEW randomIdTournament;
+DROP VIEW allPlayersInTournament;
+DROP VIEW playersRanking;
+DROP VIEW average;
--- Entrega3/queries/int10.sql
+++ Entrega3/queries/int10.sql
@@ -1,5 +1,8 @@
 .mode columns
 .headers on
 .nullvalue NULL
--- a maior diferenca de pontos fide entre jogadores num qualquer torneio, usar abs para diferencas positivas 
 
+-- Mostra o clube com o pior ranking.
+
+SELECT MIN(ranking) as PointsWorstClub, name as ClubName
+FROM ChessClub;
