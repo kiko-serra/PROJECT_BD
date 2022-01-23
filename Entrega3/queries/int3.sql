ac331adc3b93e7b5ae80c90ffb118834f033109f
kiko-serra <franciscopimentelserra@gmail.com>
2022-01-23 16:36:23 +0000

    Int 3 done.

--- Entrega3/queries/int3.sql
+++ Entrega3/queries/int3.sql
@@ -2,12 +2,20 @@
 .headers on
 .nullvalue NULL
 
---Listar o numero de jogos em que os jogadores tinham titulos de fide. Com "grandMaster" quantos jogos houve?
---tentativa de apresentar um id do jogo e o id de cada um dos players
+--List each Match with both Players id's and their Fide title.
 
-SELECT id_match, id_player, count(id_match) as um
-From Match join PlayerMatch using(id_match)
-group by id_player
-order by id_match asc;
+CREATE VIEW IF NOT EXISTS TitlePlayer as
+    SELECT title, id_player 
+    FROM Player join FideRanking using(id_player);
 
+CREATE VIEW IF NOT EXISTS MatchIndex as
+    SELECT id_match, id_player, title
+    FROM PlayerMatch join TitlePlayer using(id_player);
 
+SELECT P1.id_match as id_match, P1.id_player as player1, P1.title as title_p1, P2.id_player as player2, P2.title as title_p2
+FROM MatchIndex P1, MatchIndex P2
+WHERE P1.id_match = P2.id_match and P1.id_player <  P2.id_player
+Order by id_match;
+
+DROP VIEW TitlePlayer;
+DROP VIEW MatchIndex;
