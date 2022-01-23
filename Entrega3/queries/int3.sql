3ca3401adce6230aae4274bfe6fb9c3e7e8aa45b
kiko-serra <franciscopimentelserra@gmail.com>
2022-01-23 00:20:58 +0000

    Trying to implement query 3 but life is hard

--- Entrega3/queries/int3.sql
+++ Entrega3/queries/int3.sql
@@ -2,6 +2,12 @@
 .headers on
 .nullvalue NULL
 
--- Listagem de número de partidas de cada player versão 2
+--Listar o numero de jogos em que os jogadores tinham titulos de fide. Com "grandMaster" quantos jogos houve?
+--tentativa de apresentar um id do jogo e o id de cada um dos players
+
+SELECT id_match, id_player, count(id_match) as um
+From Match join PlayerMatch using(id_match)
+group by id_player
+order by id_match asc;
 
 
