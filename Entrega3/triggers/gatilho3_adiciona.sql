bfea372567ecb56ccd6a1740d5d37324dedead6c
FranciscoAraujoPT <quico.calado.araujo@gmail.com>
2022-01-24 16:49:56 +0000

    Second  try in triggers.

--- Entrega3/triggers/gatilho3_adiciona.sql
+++ Entrega3/triggers/gatilho3_adiciona.sql
@@ -7,7 +7,7 @@ PRAGMA foreign_keys = ON;
 CREATE TRIGGER IF NOT EXISTS WrongCategoryAdults 
     AFTER INSERT ON FideRanking
     FOR EACH ROW
-    WHEN (SELECT birthday FROM Player WHERE julianday('now') - julianday(birthday)) >= 12000.0
+    WHEN (SELECT julianday('now') - julianday(birthday) FROM Player) >= 12000.0 
     BEGIN
         UPDATE FideRanking SET category = "MEN" WHERE category = "JUNIORS";
         UPDATE FideRanking SET category = "WOMEN" WHERE category = "GIRLS";
@@ -16,8 +16,8 @@ CREATE TRIGGER IF NOT EXISTS WrongCategoryAdults
 CREATE TRIGGER IF NOT EXISTS WrongCategoryChilds 
     AFTER INSERT ON FideRanking
     FOR EACH ROW
-    WHEN (SELECT birthday FROM Player WHERE julianday('now') - julianday(birthday)) < 12000.0
+    WHEN (SELECT julianday('now') - julianday(birthday) FROM Player) < 12000.0
     BEGIN
-        UPDATE FideRanking SET category = "JUNIORS" WHERE category = "MEN";
-        UPDATE FideRanking SET category = "GIRLS" WHERE category = "WOMEN";
+        UPDATE FideRanking SET category = "JUNIORS" WHERE NEW.category = "MEN";
+        UPDATE FideRanking SET category = "GIRLS" WHERE NEW.category = "WOMEN";
     END;
