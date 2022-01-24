6497a207a5456b8e34d786fc29aea973191e9984
FranciscoAraujoPT <quico.calado.araujo@gmail.com>
2022-01-24 13:55:36 +0000

    First try in triggers.

--- Entrega3/triggers/gatilho2_adiciona.sql
+++ Entrega3/triggers/gatilho2_adiciona.sql
@@ -5,3 +5,11 @@
 PRAGMA foreign_keys = ON;
 
 --
+Create Trigger IF NOT EXISTS UpdateMembers
+After Insert on MemberId
+For Each Row
+When exists (select Player.id_player from Player where Player.id_player = New.id_player)
+Begin
+    UPDATE ChessClub SET number_of_members = number_of_members + 1
+    WHERE (SELECT MemberId.id_club FROM MemberId) = ChessClub.id_club;
+End;
--- Entrega3/triggers/gatilho3_adiciona.sql
+++ Entrega3/triggers/gatilho3_adiciona.sql
@@ -3,3 +3,21 @@
 .nullvalue NULL
 
 PRAGMA foreign_keys = ON;
+
+CREATE TRIGGER IF NOT EXISTS WrongCategoryAdults 
+    AFTER INSERT ON FideRanking
+    FOR EACH ROW
+    WHEN (SELECT birthday FROM Player WHERE julianday('now') - julianday(birthday)) >= 12000.0
+    BEGIN
+        UPDATE FideRanking SET category = "MEN" WHERE category = "JUNIORS";
+        UPDATE FideRanking SET category = "WOMEN" WHERE category = "GIRLS";
+    END;
+
+CREATE TRIGGER IF NOT EXISTS WrongCategoryChilds 
+    AFTER INSERT ON FideRanking
+    FOR EACH ROW
+    WHEN (SELECT birthday FROM Player WHERE julianday('now') - julianday(birthday)) < 12000.0
+    BEGIN
+        UPDATE FideRanking SET category = "JUNIORS" WHERE category = "MEN";
+        UPDATE FideRanking SET category = "GIRLS" WHERE category = "WOMEN";
+    END;
