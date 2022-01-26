faceb4817b460c55439ef585d35f13480b82cdde
FranciscoAraujoPT <quico.calado.araujo@gmail.com>
2022-01-26 16:32:42 +0000

    Checking Everthing Part 5

--- Entrega3/triggers/gatilho3_adiciona.sql
+++ Entrega3/triggers/gatilho3_adiciona.sql
@@ -8,18 +8,16 @@ PRAGMA foreign_keys = ON;
 
 CREATE TRIGGER IF NOT EXISTS WrongCategoryAdults 
     AFTER INSERT ON FideRanking
-    FOR EACH ROW
-    WHEN (SELECT julianday('now') - julianday(birthday) FROM Player) >= 12000.0 
+    WHEN (SELECT julianday('now') - julianday(birthday) FROM Player WHERE id_player = NEW.id_player) >= 12000.0 
     BEGIN
-        UPDATE FideRanking SET category = "MEN" WHERE NEW.category = "JUNIORS" and id_player = NEW.id_player;
-        UPDATE FideRanking SET category = "WOMEN" WHERE NEW.category = "GIRLS" and id_player = NEW.id_player;
+        UPDATE FideRanking SET category = "MEN" WHERE NEW.category = "JUNIORS" AND id_player = NEW.id_player;
+        UPDATE FideRanking SET category = "WOMEN" WHERE NEW.category = "GIRLS" AND id_player = NEW.id_player;
     END;
 
 CREATE TRIGGER IF NOT EXISTS WrongCategoryChilds 
     AFTER INSERT ON FideRanking
-    FOR EACH ROW
-    WHEN (SELECT julianday('now') - julianday(birthday) FROM Player) < 12000.0
+    WHEN (SELECT julianday('now') - julianday(birthday) FROM Player WHERE id_player = NEW.id_player) < 12000.0
     BEGIN
-        UPDATE FideRanking SET category = "JUNIORS" WHERE NEW.category = "MEN" and id_player = NEW.id_player;
-        UPDATE FideRanking SET category = "GIRLS" WHERE NEW.category = "WOMEN" and id_player = NEW.id_player;
+        UPDATE FideRanking SET category = "JUNIORS" WHERE NEW.category = "MEN" AND id_player = NEW.id_player;
+        UPDATE FideRanking SET category = "GIRLS" WHERE NEW.category = "WOMEN" AND id_player = NEW.id_player;
     END;
